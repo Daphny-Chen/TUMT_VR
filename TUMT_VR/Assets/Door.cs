99559ae12c19086160c2dcdf5e5c861a6d292b07
Daphny-Chen <55218517+Daphny-Chen@users.noreply.github.com>
2019-12-19 11:20:13 +0800

    1219

---
+++ TUMT_VR/Assets/Door.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+
+    public Animator animtorControl;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "HeadCollider"&&Game.havekey==true) {
+
+            animtorControl.SetBool("character_nearby",true);
+        }
+    }
+
+}
---
+++ TUMT_VR/Assets/Game.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public static bool havekey;
+
+
+    public void ThrowPickUp() {
+        print("");
+    }
+
+    public void ThrowDetach(GameObject buj)
+    {
+        print("");
+        havekey = true;
+        Destroy(buj);
+    }
+
+
+
+    public void ThrowHeld()
+    {
+        print("");
+    }
+
+
+
+
+}
